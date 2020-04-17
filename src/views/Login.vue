e5aa29a83eacf34f1cf3a4e50602265d957d7892
tusharg1999 <tusharg1999>
2020-04-18 02:42:28 +0530

    login view ui

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="container">
+        <div class="main">
+            <div class="form-div">
+                <h1>Sign in to Account</h1>
+                <form>
+                    <label>
+                        <input placeholder=" ">
+                        <span>Placeholder Text</span>
+                    </label><br/>
+                    <label>
+                        <input placeholder=" ">
+                        <span>Placeholder Text</span>
+                    </label><br/>
+                    <button>Sign In</button>
+                </form>
+            </div>
+            <div class="design-div"></div>
+        </div>
+    </section>
+</template>
+<style lang="scss">
+    @import "src/sass/Login/main";
+</style>
